30988f1b1ac9f4465b60d908ccdd18f6d292052f
Pierre Brethes <pi.brethes@gmail.com>
2016-11-03 09:03:10 +0100

    new model

---
+++ api/controllers/UserGroupCameraController.js
@@ -0,0 +1,11 @@
+/**
+ * UserGroupCameraController
+ *
+ * @description :: Server-side logic for managing Usergroupcameras
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
